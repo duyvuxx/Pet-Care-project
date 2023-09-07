d9d96f3d0fde217d96d0968890cb6d385e09442e
duyvuxx <duy.vux88@gmail.com>
2023-09-07 22:05:06 +0700

    done_service_detail

--- assets/js/service_detail.js
+++ assets/js/service_detail.js
@@ -4,12 +4,33 @@ const dataValueTitle = urlParams.get("dataTitle");
 const dataValueImg = urlParams.get("dataImg");
 
 const detailImg = $(".detail-left-wrapper > img");
-const detailTitle = $(".detail-left-title > h2");
+const detailTitle = $(".detail-left-title > .first-infor > h2");
 
 function handleDetailService(val, num) {
-  $(detailImg[0]).attr("src", `./assets/imgs/main/main_img_${num}.png`);
+  $(detailImg[0]).attr("src", `./assets/imgs/main/main_service_${num}.jpg`);
   $(detailImg[1]).attr("src", `./assets/imgs/main/main_sub_img_${num}.svg`);
   $(detailTitle).text(`${val}`);
 }
 
 handleDetailService(dataValueTitle, dataValueImg);
+
+// Detail List
+// function changeList() {
+//   const item = $(".detail-right-list-items");
+
+//   item.each((idx, val) => {
+//     $(val).preventDefault().click(() => {
+//       console.log(idx);
+//       // $(detailImg[0]).attr(
+//       //   "src",
+//       //   `./assets/imgs/main/main_service_${idx + 1}.jpg`
+//       // );
+//       // $(detailImg[1]).attr(
+//       //   "src",
+//       //   `./assets/imgs/main/main_sub_img_${idx + 1}.svg`
+//       // );
+//     });
+//   });
+// }
+
+// changeList();
